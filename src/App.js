d4b7a1aafc343be5934046916d45a7f9c7c92bee
Eisenhardt88 <eisenhardt88@yahoo.com>
2021-01-28 17:24:06 +0200

    add useDebounce

--- src/App.js
+++ src/App.js
@@ -6,6 +6,34 @@ function App() {
   const [userList, setUserList] = useState([]);
   const [perPage, setPerPage] = useState(10);
 
+  const [isSearching, setIsSearching] = useState(false);
+  const debouncedPerPage = useDebounce(perPage, 1000);
+
+  function useDebounce(value, delay) {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+    useEffect(() => {
+      const handler = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
+      return () => {
+        clearTimeout(handler);
+      };
+    }, [value, delay]);
+    return debouncedValue;
+  }
+
+  useEffect(() => {
+    if (debouncedPerPage) {
+      setIsSearching(true);
+      getUsersListApi(debouncedPerPage).then((userList) => {
+        setIsSearching(false);
+        setUserList(userList);
+      });
+    } else {
+      setUserList([]);
+    }
+  }, [debouncedPerPage]);
+
   useEffect(() => {
     const params = {
       perPage: perPage,
@@ -29,6 +57,7 @@ function App() {
         onChange={(e) => setPerPage(e.target.value)}
         value={perPage}
       ></input>
+      {isSearching && <div>Searching ...</div>}
       {userList.map((el) => (
         <div>{el.login}</div>
       ))}
